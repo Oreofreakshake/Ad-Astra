dd94b50721ef0ff89e8af8a980f0fcb31129f4bf
Oreofreakshake <53213763+Oreofreakshake@users.noreply.github.com>
2022-02-25 01:09:48 +0500

    added tailwind

---
+++ src/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+
+const Home = () => {
+    return (
+        <div className="bg-blue-400 p-5">
+            <Navbar />
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className="text-3xl font-bold text-green-800 justify-center flex">Hello im the Navbar</div>
+    );
+};
+
+export default Navbar;
