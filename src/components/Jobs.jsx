0a6800859f1a5d6d8231f96455ab46217b9e9a00
Oreofreakshake <53213763+Oreofreakshake@users.noreply.github.com>
2022-02-26 10:16:32 +0500

    added job cards

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,7 +1,79 @@
-import React from 'react';
+import React from "react";
 
 const Jobs = () => {
-  return <div className='text-indigo-900'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, optio tenetur fugit laudantium harum, quam obcaecati quasi, nobis suscipit expedita dolores exercitationem at dignissimos voluptatibus ullam ab perferendis veniam minus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum obcaecati similique ea doloremque quidem laboriosam dolor autem dolore architecto officia voluptatibus amet aspernatur illo, molestiae neque quam quis, incidunt maxime? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda officia omnis repellat et! Harum obcaecati magni dolorem, nobis quaerat illo officiis velit at laudantium ab, laboriosam consequatur fugit consequuntur delectus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt illum non, expedita, illo quod, ratione blanditiis saepe vero inventore consectetur atque ipsum nulla natus qui nobis minima nisi! Aliquid, non? Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quo vitae eos natus reprehenderit nostrum voluptatibus magni? Voluptate ipsa consectetur possimus quia, reiciendis explicabo laboriosam dolore aspernatur ullam accusamus earum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat harum, tempore impedit possimus molestiae, in, suscipit quo quia quis nemo adipisci eum iste iusto? Voluptas, delectus sint? Veniam, exercitationem aliquid.Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, optio tenetur fugit laudantium harum, quam obcaecati quasi, nobis suscipit expedita dolores exercitationem at dignissimos voluptatibus ullam ab perferendis veniam minus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum obcaecati similique ea doloremque quidem laboriosam dolor autem dolore architecto officia voluptatibus amet aspernatur illo, molestiae neque quam quis, incidunt maxime? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda officia omnis repellat et! Harum obcaecati magni dolorem, nobis quaerat illo officiis velit at laudantium ab, laboriosam consequatur fugit consequuntur delectus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt illum non, expedita, illo quod, ratione blanditiis saepe vero inventore consectetur atque ipsum nulla natus qui nobis minima nisi! Aliquid, non? Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quo vitae eos natus reprehenderit nostrum voluptatibus magni? Voluptate ipsa consectetur possimus quia, reiciendis explicabo laboriosam dolore aspernatur ullam accusamus earum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat harum, tempore impedit possimus molestiae, in, suscipit quo quia quis nemo adipisci eum iste iusto? Voluptas, delectus sint? Veniam, exercitationem aliquid.Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, optio tenetur fugit laudantium harum, quam obcaecati quasi, nobis suscipit expedita dolores exercitationem at dignissimos voluptatibus ullam ab perferendis veniam minus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum obcaecati similique ea doloremque quidem laboriosam dolor autem dolore architecto officia voluptatibus amet aspernatur illo, molestiae neque quam quis, incidunt maxime? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda officia omnis repellat et! Harum obcaecati magni dolorem, nobis quaerat illo officiis velit at laudantium ab, laboriosam consequatur fugit consequuntur delectus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt illum non, expedita, illo quod, ratione blanditiis saepe vero inventore consectetur atque ipsum nulla natus qui nobis minima nisi! Aliquid, non? Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quo vitae eos natus reprehenderit nostrum voluptatibus magni? Voluptate ipsa consectetur possimus quia, reiciendis explicabo laboriosam dolore aspernatur ullam accusamus earum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat harum, tempore impedit possimus molestiae, in, suscipit quo quia quis nemo adipisci eum iste iusto? Voluptas, delectus sint? Veniam, exercitationem aliquid.Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, optio tenetur fugit laudantium harum, quam obcaecati quasi, nobis suscipit expedita dolores exercitationem at dignissimos voluptatibus ullam ab perferendis veniam minus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum obcaecati similique ea doloremque quidem laboriosam dolor autem dolore architecto officia voluptatibus amet aspernatur illo, molestiae neque quam quis, incidunt maxime? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda officia omnis repellat et! Harum obcaecati magni dolorem, nobis quaerat illo officiis velit at laudantium ab, laboriosam consequatur fugit consequuntur delectus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt illum non, expedita, illo quod, ratione blanditiis saepe vero inventore consectetur atque ipsum nulla natus qui nobis minima nisi! Aliquid, non? Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quo vitae eos natus reprehenderit nostrum voluptatibus magni? Voluptate ipsa consectetur possimus quia, reiciendis explicabo laboriosam dolore aspernatur ullam accusamus earum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat harum, tempore impedit possimus molestiae, in, suscipit quo quia quis nemo adipisci eum iste iusto? Voluptas, delectus sint? Veniam, exercitationem aliquid.</div>;
+    return (
+        <div>
+            <div>
+                <div className="flex p-24 justify-center align-middle">
+                    <div className="w-56 h-52 bg-indigo-400 hover:bg-indigo-600 cursor-pointer rounded-2xl mr-14">
+                        <div className="p-12 w-full h-9 text-center font-bold text-3xl text-white mb-3">
+                            Private
+                        </div>
+                        <div className="w-full h-10 flex justify-center align-middle">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="w-20 h-20"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="white"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                                />
+                            </svg>
+                        </div>
+                    </div>
+                    <div className="w-56 h-52 bg-indigo-400 hover:bg-indigo-600 cursor-pointer rounded-2xl">
+                        <div className="p-12 w-full h-10 text-center font-bold text-3xl text-white mb-3">
+                            Civil
+                        </div>
+                        <div className="w-full h-10 flex justify-center align-middle">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="w-20 h-20"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="white"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                                />
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex pb-24 justify-center align-top">
+                <div className="w-56 h-52 bg-indigo-400 hover:bg-indigo-600 cursor-pointer rounded-2xl">
+                        <div className="p-12 w-full h-9 text-center font-bold text-3xl text-white mb-3">
+                            Resorts
+                        </div>
+                        <div className="w-full h-10 flex justify-center align-middle">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="w-20 h-20"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="white"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                                />
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Jobs;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,9 @@ const Navbar = () => {
         <div className=" flex justify-between">
             <div className="p-3 w-44 font-bold text-3xl text-white">Ad Astra</div>
             <div className="flex justify-between align-to p-2">
-                <Buttons>Jobs</Buttons>
+                <Text>Jobs</Text>
                 <Buttons>Get hired</Buttons>
-                <Buttons>News</Buttons>
+                <Text>News</Text>
                 <Buttons>Start hiring</Buttons>
             </div>
         </div>
@@ -18,7 +18,15 @@ const Navbar = () => {
 
 const Buttons = (props) => {
     return (
-        <div className=" bg-gradient-to-b from-indigo-400 to-indigo-500 shadow-none flex justify-center p-3 w-29 h-12 cursor-pointer font-bold text-base rounded-lg text-white hover:shadow-md mr-3 hover:from-indigo-800 hover:to-indigo-900">
+        <div className=" bg-gradient-to-b from-indigo-400 to-indigo-500 shadow-none flex justify-center p-3 w-29 h-12 cursor-pointer font-semibold text-base rounded-lg text-white hover:shadow-md mr-3 hover:from-indigo-800 hover:to-indigo-900">
+            {props.children}
+        </div>
+    );
+};
+
+const Text = (props) => {
+    return (
+        <div className=" shadow-none flex justify-center p-3 w-29 h-12 cursor-pointer font-semibold text-base rounded-lg text-white hover:text-indigo-900 mr-3 ">
             {props.children}
         </div>
     );
